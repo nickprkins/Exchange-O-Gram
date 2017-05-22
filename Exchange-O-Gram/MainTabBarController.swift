49e54068122a24c1f7dabd60bf7ca9b1e2825d6c
nickprkins <nickperkins@protonmail.com>
2017-05-22 14:58:18 -0400

    began creating UserProfileController

---
+++ Exchange-O-Gram/MainTabBarController.swift
@@ -0,0 +1,29 @@
+//
+//  MainTabBarController.swift
+//  Exchange-O-Gram
+//
+//  Created by Nick Perkins on 5/22/17.
+//  Copyright © 2017 Nicholas Perkins. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let layout = UICollectionViewFlowLayout()
+        let userProfileController = UserProfileController(collectionViewLayout: layout)
+        
+        let navController = UINavigationController(rootViewController: userProfileController)
+        navController.tabBarItem.image = #imageLiteral(resourceName: "profile_unselected")
+        navController.tabBarItem.selectedImage = #imageLiteral(resourceName: "profile_selected")
+        
+        tabBar.tintColor = .black
+        
+        viewControllers = [navController, UIViewController()]
+        
+    }
+    
+}
